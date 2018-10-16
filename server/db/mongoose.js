402e6c564a85ec7e9b068d71c5e2559a000c35b7
Mark Shull <shullski@MacBook-Pro-6.lan.internal.airsquirrels.com>
2018-10-16 11:17:51 -0400

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 // This sets mongoose up to use promises
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Todos');
+mongoose.connect('mongodb://localhost:27017/Todos', { useNewUrlParser: true });
 
 module.exports = {
   mongoose,
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) => {
     res.send(doc);
-    console.log(doc);
+    // console.log(doc);
   }, (err) => {
     res.status(400).send(err);
   });
@@ -23,3 +23,6 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+
+module.exports = {app};
